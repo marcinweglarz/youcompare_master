0549acbb709ab5aa7b3e870f560cf952b5bfbdc6
Marcin Weglarz <marcin.weglarz@hotmail.com>
2011-12-21 00:21:22 +0000

    Client registration form

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -3,11 +3,21 @@ class ClientsController < ApplicationController
     @client = Client.new
   end
 
+   def show
+    @client = Client.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml { render :xml => @client }
+    end
+  end
+
   def create
     @client = Client.new(params[:client])
     respond_to do |format|
       if @client.save
-        format.html { redirect_to(@client, :notice => 'Client registered successful!') }
+        format.html { redirect_to(@client, :notice => 'Thank you very much for registering your account with us.
+A confirmation email was sent to you. Please note, your registration is subject to the Administrator approval.') }
         format.xml { render :xml => @client, :status => :created, :location => @client }
       else
         format.html { render :action => "new" }
