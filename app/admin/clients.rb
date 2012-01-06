6c0ebae1bb598a8f6e0edae24bcd938989947ce2
andrzej-poniatowski <poniatowski.andrzej@gmail.com>
2012-01-06 06:40:53 +0000

    references2

--- app/admin/clients.rb
+++ app/admin/clients.rb
@@ -1,5 +1,5 @@
 #@author Andrzej Poniatowski  student ID x11106077.
-#@author Marcin Wenglarz Student ID x11106832
+#@author Marcin Weglarz Student ID x11106832
 #@date 05.01.212
 #@version Rev 1
 #@references http://net.tutsplus.com/tutorials/ruby/create-beautiful-administration-interfaces-with-active-admin/
